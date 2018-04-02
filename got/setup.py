79c94415099ef92eaddfe82a54214981cec28331
@be <abeahmed2@gmail.com>
2018-04-02 09:40:05 -0400

    Refactored project setup to allow pip installs.

---
+++ got/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='got',
+      version='0.1',
+      author='Ibrahim Ahmed',
+      author_email='abeahmed2@gmail.com',
+      license='MIT',
+      packages=['got'],
+      zip_safe=False)
---
+++ got3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='got3',
+      version='0.1',
+      author='Ibrahim Ahmed',
+      author_email='abeahmed2@gmail.com',
+      license='MIT',
+      packages=['got3'],
+      zip_safe=False)
